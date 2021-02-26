4d16a3b3513ba68c7045aebf7e22b1c3d058b56d
David Landsman <david.landsman@mail.utoronto.ca>
2021-02-25 19:36:48 -0500

    Work on fixing E/M step of Roberts

--- src/ffa_roberts.py
+++ src/ffa_roberts.py
@@ -8,30 +8,28 @@ import numpy as np
 Z = np.loadtxt(open("../data/exams_imputed.csv", "rb"), delimiter=",")
 N, K = Z.shape
 P = 2
-iter = 100
+iter = 300
 tol = 0.0001
 
-# X = X - np.ones((N, 1)) * np.mean(X, axis=0)
-mu = np.mean(Z, axis=0)
-ZZ = np.matmul(Z.T, Z) / N
-diagZZ = np.diag(ZZ)
 
-covZ = np.cov(Z.T)
-scale = np.linalg.det(covZ) ** (1 / K)
-Lambda = np.random.randn(K, P) * np.math.sqrt(scale / P)
-Sigma = np.diag(covZ)
+# parameters
+mu = np.expand_dims(np.array([40.51, 51.91, 51.82, 49.32, 44.36]), axis=-1) # initial values from Roberts
+lam = np.ones((K, P)) # \lambda
+Lam = np.hstack((lam, mu)) # \Lambda = [\lambda, \mu]
+Sigma = np.identity(K)
 
-# build Y by computing y_t = H_t*z_t for each observation t
-H = [np.identity(K)] * N # TODO: replace with real missingness structure
-Y = []
-for t in range(N):
-    # y_t = np.matmul(H[t], Z[t])
-    y_t = H[t] @ Z[t]
-    Y.append(y_t)
+I_p = np.identity(P)
 
-Y = np.array(Y)
+# # build Y by computing y_t = H_t*z_t for each observation t
+# H = [np.identity(K)] * N # TODO: replace with real missingness structure
+# Y = []
+# for t in range(N):
+#     # y_t = np.matmul(H[t], Z[t])
+#     y_t = H[t] @ Z[t]
+#     Y.append(y_t)
 
-I = np.identity(P)
+# Y = np.array(Y)
+
 
 lik = 0
 LL = []
@@ -39,38 +37,39 @@ const = -K / 2 * np.math.log(2 * np.math.pi)
 
 for i in range(iter):
     # E Step
-    # R_det = np.diag(1 / R)
-    # LR_det = np.matmul(R_det, Lambda)
-    # left_mat = np.matmul(LR_det, np.linalg.inv(I + np.matmul(Lambda.T, LR_det)))
-    # MM = R_det - np.matmul(left_mat, LR_det.T)
-    R = Lambda @ Lambda.T + np.diag(Sigma)
-    Lambda_R = Lambda.T @ np.linalg.inv(R)
-    EX = Lambda_R @ (Y - np.ones((N, 1)) * mu).T
-    EXtilde = np.vstack([EX, np.ones(N)])
-    EXtilde_Z = EXtilde @ Z
+    # compute R^-1 = (\Lambda\Lambda^T + \Sigma)^-1
+    Sigma_inv = np.diag(np.diag(1 / Sigma))
+    S_lam = Sigma_inv @ lam
+    Q = np.linalg.inv(I_p + lam.T @ S_lam)
+    R = Sigma + lam @ lam.T
+    R_inv = Sigma_inv - S_lam @ Q @ S_lam.T # Woodbury identity
 
-    EXX = I - Lambda_R @ Lambda
-    # add EXX as square submatrix of EXXtilde
-    EXXtilde = np.hstack([EXX, np.zeros((P, 1))])
-    EXXtilde = np.vstack([EXXtilde, np.zeros(P+1)])
-    # dM = np.math.sqrt(np.linalg.det(MM))
-    # beta = np.matmul(Lambda.T, MM)
-    # XXbeta = np.matmul(ZZ, beta.T)
-    # EZZ = I - np.matmul(beta, Lambda) + np.matmul(beta, XXbeta)
+    Z_cent = Z - mu.T # centered Z (data)
 
-    MM = np.linalg.inv(R)
-    dM = np.math.sqrt(np.linalg.det(MM))
+    beta = lam.T @ R_inv
+    Ex_z = beta @ Z_cent.T
+    Exx_z = I_p - beta @ lam + Ex_z @ Ex_z.T
+    # first moment of \tilde{x}|z
+    Extilde_z = np.vstack( (beta @ Z_cent.T, np.ones(N)) )
+    # variance of \tilde{x}|z
+    Vxtilde_z = np.hstack( (Exx_z, np.zeros((P, 1))) )
+    Vxtilde_z = np.vstack( (Vxtilde_z, np.zeros(P+1)) )
+    # second moment of \tilde{x}|z = Var + E\tilde{x}|z ^ 2
+    Exxtilde_z = Vxtilde_z + Extilde_z @ Extilde_z.T
 
     # compute LL
     old_lik = lik
-    lik = N * const + N * np.math.log(dM) - 0.5 * N * np.sum(np.diag(np.matmul(MM, ZZ)))
+    lik = \
+        - 0.5 * N * np.math.log(np.linalg.det(R)) \
+        - 0.5 * N * K * np.math.log(2 * np.math.pi) \
+        - 0.5 * np.sum(np.diag(R_inv @ Z_cent.T @ Z_cent))
 
     print(f'cycle {i} lik {lik}\n')
     LL.append(lik)
 
     # M step
-    Lambda = np.matmul(EXtilde_Z, np.linalg.inv(EXXtilde))
-    R = diagZZ - np.diag(np.matmul(Lambda, EXtilde_Z))
+    Lam = Z.T @ Extilde_z.T @ np.linalg.inv(Exxtilde_z)
+    Sigma = np.diag(np.diag(Z.T @ Z - Lam @ Extilde_z @ Z))
 
     if i <= 2:
         lik_base = lik
